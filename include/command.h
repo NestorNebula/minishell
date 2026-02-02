843d84e3e0d19d246b03e992719fecbf5a529e5b
Noa Houssier <nhoussie@student.42.fr>
2026-02-02 14:27:00 +0100

    feat: add command module's header

---
+++ include/command.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   command.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/02 14:25:59 by nhoussie          #+#    #+#             */
+/*   Updated: 2026/02/02 14:28:07 by nhoussie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COMMAND_H
+# define COMMAND_H
+
+# include "dll.h"
+
+typedef int	t_pipe[2];
+
+typedef struct s_command {
+	const char			**raw_argv;
+	char				**args;
+	char				*filepath;
+	int					pid;
+	int					wstatus;
+	t_dll				*in_fds;
+	t_dll				*out_fds;
+	t_pipe				pipe;
+}				t_command;
+
+/**
+ * Initializes a new command structure.
+ *
+ * @param raw_argv The initial args that will be used to create the command
+ * @return A pointer to a command structure on success, NULL on error
+ */
+t_command	*new_command(const char **raw_argv);
+
+/**
+ * Frees a command structure.
+ *
+ * @param command A pointer to a command structure
+ */
+void		free_command(void *command);
+
+#endif // !COMMAND_H
