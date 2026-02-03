7b3fd13040ad595d70cce66856523d153d9e6548
Noa Houssier <nhoussie@student.42.fr>
2026-02-03 12:40:34 +0100

    test: add tests for set_env_var funcion

--- tests/src/env.c
+++ tests/src/env.c
@@ -4,6 +4,7 @@
 
 #define KEY "key"
 #define VALUE "value"
+#define NEW_VALUE "new value"
 
 void	new_env_var_test(void)
 {
@@ -50,8 +51,37 @@ void	find_env_var_test(void)
 	end_unit_test(unit_test);
 }
 
+void	set_env_var_test(void)
+{
+	t_unit_test	*unit_test;
+	t_dll		*env_vars;
+	t_env_var	*env_var;
+
+	unit_test = new_unit_test("set_env_var", false);
+	env_vars = NULL;
+	set_env_var(&env_vars, KEY, VALUE);
+	cut_assert(env_vars != NULL, unit_test,
+		"adds new variable to the list when key doesn't exist");
+	env_var = find_env_var(env_vars, KEY);
+	cut_assert(env_var != NULL && strcmp(env_var->key, KEY) == 0, unit_test,
+		"stores given key in new env_var structure");
+	cut_expect(strcmp(env_var->value, VALUE) == 0, unit_test,
+		"stores given value in new env_var structure");
+	set_env_var(&env_vars, KEY, NEW_VALUE);
+	env_var = find_env_var(env_vars, KEY);
+	cut_assert(env_var != NULL, unit_test,
+		"doesn't delete variable when trying to update it");
+	cut_expect(dll_size(env_vars) == 1, unit_test,
+		"only updates variale when given key that already exists");
+	cut_expect(strcmp(env_var->value, NEW_VALUE) == 0, unit_test,
+		"updates variable with given value");
+	clear_dll(&env_vars, free_env_var);
+	end_unit_test(unit_test);
+}
+
 int	main(void)
 {
 	new_env_var_test();
 	find_env_var_test();
+	set_env_var_test();
 }
