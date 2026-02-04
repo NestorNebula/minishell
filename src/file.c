b4cc94f33483eb4a315cc3cfe8ae8f0ad886f867
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-04 10:24:02 +0100

    feat: allow file to have NULL path

--- src/file.c
+++ src/file.c
@@ -6,7 +6,7 @@
 /*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 18:31:51 by nhoussie          #+#    #+#             */
-/*   Updated: 2026/02/04 10:18:33 by nhoussie         ###   ########.fr       */
+/*   Updated: 2026/02/04 10:23:08 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,18 @@ t_file	*new_file(const char *path, int fd, t_file_status status)
 {
 	t_file	*file;
 
-	if (path == NULL)
-		return (NULL);
 	file = malloc(sizeof(t_file));
 	if (file == NULL)
 		return (NULL);
-	file->path = ft_strdup(path);
-	if (file->path == NULL)
+	file->path = NULL;
+	if (path != NULL)
 	{
-		free(file);
-		return (NULL);
+		file->path = ft_strdup(path);
+		if (file->path == NULL)
+		{
+			free(file);
+			return (NULL);
+		}
 	}
 	file->fd = fd;
 	file->status = status;
