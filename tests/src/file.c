420ab06917a9ef340188bac2ef8d3b5fb552b21e
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-04 15:34:25 +0100

    test: refactor test due to file structure changes

--- tests/src/file.c
+++ tests/src/file.c
@@ -10,15 +10,15 @@ void	new_file_test(void)
 	t_file		*file;
 
 	unit_test = new_unit_test("new_file", false);
-	file = new_file(PATH, 0, FILE_DEFAULT);
+	file = new_file(PATH, FILE_REG);
 	cut_assert(file != NULL, unit_test,
 		"returns non-null pointer on file structure");
 	cut_expect(strcmp(file->path, PATH) == 0, unit_test,
 		"stores path argument as file's path");
-	cut_expect(file->fd == 0, unit_test,
-		"stores fd argument as file's fd");
-	cut_expect(file->status == FILE_DEFAULT, unit_test,
-		"stores status argument as file's status");
+	cut_expect(file->status == FILE_DEF, unit_test,
+		"sets file's status to default");
+	cut_expect(file->type == FILE_REG, unit_test,
+		"stores type argument as file's type ");
 	free_file(file);
 	end_unit_test(unit_test);
 }
