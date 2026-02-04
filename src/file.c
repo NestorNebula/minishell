7be274aaf53be49919f6bb540374ba48a94496f3
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-04 10:20:47 +0100

    feat: update fd module into file module

--- src/file.c
+++ src/file.c
@@ -1,31 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fd.c                                               :+:      :+:    :+:   */
+/*   file.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 18:31:51 by nhoussie          #+#    #+#             */
-/*   Updated: 2026/02/02 18:31:52 by nhoussie         ###   ########.fr       */
+/*   Updated: 2026/02/04 10:18:33 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include "fd.h"
+#include "file.h"
+#include "libft.h"
 
-t_fd	*new_fd(int fd, int status)
+t_file	*new_file(const char *path, int fd, t_file_status status)
 {
-	t_fd	*fd_ptr;
+	t_file	*file;
 
-	fd_ptr = malloc(sizeof(t_fd));
-	if (fd_ptr == NULL)
+	if (path == NULL)
 		return (NULL);
-	fd_ptr->fd = fd;
-	fd_ptr->status = status;
-	return (fd_ptr);
+	file = malloc(sizeof(t_file));
+	if (file == NULL)
+		return (NULL);
+	file->path = ft_strdup(path);
+	if (file->path == NULL)
+	{
+		free(file);
+		return (NULL);
+	}
+	file->fd = fd;
+	file->status = status;
+	return (file);
 }
 
-void	free_fd(void *fd)
+void		free_file(void *file)
 {
-	free(fd);
+	if (file != NULL)
+	{
+		free((void *)((t_file *) file)->path);
+		free(file);
+	}
 }
