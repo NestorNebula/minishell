1eda35d7d012678ca5c6b0c8c503b0492d1fd88e
Cedric Monmirel <cedric.monmirel@gmail.com>
2026-02-05 12:21:02 +0100

    OK

--- include/shell.h
+++ include/shell.h
@@ -6,7 +6,7 @@
 /*   By: cmonmire <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 12:11:39 by cmonmire          #+#    #+#             */
-/*   Updated: 2026/02/05 12:11:42 by cmonmire         ###   ########.fr       */
+/*   Updated: 2026/02/05 12:19:19 by cmonmire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 typedef struct s_shell
 {
-	t_dll   *env;
-	t_cmd	*cmds;
+	t_dll	*env;
+	t_dll	*cmds;
 	int		last_status;
 	int		running;
 }	t_shell;
