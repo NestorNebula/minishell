04f33ba1944e00ccb65fd7d7d4bf173e65c30a88
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-06 14:37:20 +0100

    feat: update handle_command function to use t_dll

--- src/handle-command.c
+++ src/handle-command.c
@@ -6,7 +6,7 @@
 /*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 11:37:45 by nhoussie          #+#    #+#             */
-/*   Updated: 2026/02/06 12:20:06 by nhoussie         ###   ########.fr       */
+/*   Updated: 2026/02/06 14:37:58 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,14 @@
 #include "exec.h"
 #include "process.h"
 
-int		handle_command(t_command *command, t_shell *shell)
+int		handle_command(t_dll *command_node, t_shell *shell)
 {
-	int	rc;
+	int			rc;
+	t_command	*command;
 
+	if (command_node == NULL || command_node->data == NULL)
+		return (0);
+	command = command_node->data;
 	if (pipe(command->pipe) == -1)
 		return (errno);
 	rc = prepare_files(command);
@@ -26,7 +30,7 @@ int		handle_command(t_command *command, t_shell *shell)
 		return (rc);
 	rc = fork();
 	if (rc == 0)
-		handle_child(command, shell);
+		handle_child(command_node, shell);
 	else if (rc > 0)
 	{
 		handle_parent(command, rc);
