fa4a7c60e847f65a54f2941d0584555ab2535e9f
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-06 10:23:18 +0100

    feat: add prepare/exec_command function prototypes

--- include/exec.h
+++ include/exec.h
@@ -6,7 +6,7 @@
 /*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 18:09:25 by nhoussie          #+#    #+#             */
-/*   Updated: 2026/02/05 18:41:46 by nhoussie         ###   ########.fr       */
+/*   Updated: 2026/02/06 10:23:03 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,23 @@
  * first encountered error otherwise
  */
 int		prepare_files(t_command *command);
+
+/**
+ * Prepares the given command for execution.
+ *
+ * @param command_node A pointer to a dll node containing a command structure
+ * @return 0 on success, an error code otherwise
+ */
+int		prepare_command(t_dll *command_node);
+
+/**
+ * Executes the given command.
+ *
+ * @param command A pointer to a command structure
+ * @return Doesn't return on success, the code of the error that occurred
+ * otherwise
+ */
+int		exec_command(t_command *command);
 
 /**
  * Closes all input/output files used by the given command.
