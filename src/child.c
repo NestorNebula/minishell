b1611ce83d1db4a0ef8f205a35b7fd002fec12d3
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-07 09:25:11 +0100

    feat: add close_fds function

--- src/child.c
+++ src/child.c
@@ -14,9 +14,12 @@
 #include <unistd.h>
 #include "exec.h"
 #include "env.h"
+#include "file.h"
 #include "process.h"
 
-static void	exit_from_child(int ec, t_shell *shell);
+static void	exit_from_child(int ec, t_dll *command_node, t_shell *shell);
+
+static void	close_fds(t_dll *command_node);
 
 int	handle_child(t_dll *command_node, t_shell *shell)
 {
@@ -24,23 +27,52 @@ int	handle_child(t_dll *command_node, t_shell *shell)
 	t_command	*command;
 
 	if (command_node == NULL || command_node->data == NULL || shell == NULL)
-		exit_from_child(0, shell);
+		exit_from_child(0, command_node, shell);
 	command = command_node->data;
 	close(command->pipe[0]);
 	ec = prepare_command(command_node);
 	if (ec != 0)
-		exit_from_child(ec, shell);
+		exit_from_child(ec, command_node, shell);
 	ec = exec_command(command, shell);
-	exit_from_child(ec, shell);
+	exit_from_child(ec, command_node, shell);
 	exit(ec);
 }
 
-static void	exit_from_child(int ec, t_shell *shell)
+static void	exit_from_child(int ec, t_dll *command_node, t_shell *shell)
 {
+	if (command_node != NULL)
+		close_fds(command_node);
 	if (shell != NULL)
 	{
 		clear_dll(&shell->cmds, free_command);
 		clear_dll(&shell->env, free_env_var);
 	}
 	exit(ec);
+}
+
+static void	close_fds(t_dll *command_node)
+{
+	t_command	*command;
+	t_command	*prev;
+	t_file		*last_in;
+	t_file		*last_out;
+
+	command = command_node->data;
+	prev = NULL;
+	if (command_node->prev != NULL && command_node->prev->data != NULL)
+		prev = command_node->prev->data;
+	last_in = NULL;
+	if (command->in_files != NULL)
+		last_in = dll_last(command->in_files)->data;
+	last_out = NULL;
+	if (command->out_files != NULL)
+		last_out = dll_last(command->out_files)->data;
+	if (last_in != NULL && last_in->type == FILE_PIPE && prev != NULL)
+		close(prev->pipe[0]);
+	else if (last_in != NULL && last_in->type)
+		close(last_in->fd);
+	if (last_out != NULL && last_out->type == FILE_PIPE)
+		close(command->pipe[1]);
+	else if (last_out != NULL)
+		close(last_out->fd);
 }
