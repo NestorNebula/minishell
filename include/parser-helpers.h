c24620f9587482301b62f7fdda4e7bbcef11a1cc
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-11 16:18:33 +0100

    feat: add parser-helpers module's header

---
+++ include/parser-helpers.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser-helpers.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 16:14:30 by nhoussie          #+#    #+#             */
+/*   Updated: 2026/02/11 16:16:34 by nhoussie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_HELPERS_H
+# define PARSER_HELPERS_H
+
+#include "parser.h"
+
+void	build_state_0(t_parsing_table *table);
+void	build_state_1_to_7(t_parsing_table *table);
+void	build_state_8_to_9(t_parsing_table *table);
+void	build_state_10_to_19(t_parsing_table *table);
+void	build_state_20_to_21(t_parsing_table *table);
+void	build_state_22_to_24(t_parsing_table *table);
+void	build_state_25_to_29(t_parsing_table *table);
+void	build_state_30(t_parsing_table *table);
+
+#endif // !PARSER_HELPERS_H
