63c6091d728348a3f2359b2b8d0d558017fb71a2
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-15 11:18:46 +0100

    feat: add build_commands module's header

---
+++ include/build_commands.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   build_commands.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 11:00:19 by nhoussie          #+#    #+#             */
+/*   Updated: 2026/02/14 11:00:46 by nhoussie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "dll.h"
+
+/**
+ * Creates a list of commands from a list of tokens.
+ * The function must be called with a valid list of tokens, otherwise
+ * its behavior is undefined.
+ *
+ * @param tokens A pointer to the first element of a dll of tokens
+ * @return A pointer to the first element of a dll of commands on success,
+ * a NULL pointer on error
+ */
+t_dll		*build_commands(t_dll *tokens);
+
+/**
+ * Updates i/o files for each command in a command list.
+ * Removes STD i/o from i/o list if there are other files in the list.
+ * Replaces STD i/o by pipes if other commands are in the list.
+ *
+ * @param commands A pointer to the first element of a dll of commands
+ */
+void		update_commands_files(t_dll *commands);
