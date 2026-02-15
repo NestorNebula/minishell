a9d4d0f20481915ee692b85665d4eb0ec812e007
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-15 10:43:44 +0100

    feat: set default i/o files for new command

--- src/command.c
+++ src/command.c
@@ -12,6 +12,9 @@
 
 #include <stdlib.h>
 #include "command.h"
+#include "file.h"
+
+static t_command	*set_command_default_files(t_command *command);
 
 t_command	*new_command(const char **raw_argv)
 {
@@ -25,12 +28,42 @@ t_command	*new_command(const char **raw_argv)
 	command->filepath = NULL;
 	command->pid = -1;
 	command->wstatus = -1;
-	command->in_files = NULL;
-	command->out_files = NULL;
+	if (set_command_default_files(command) == NULL)
+	{
+		free_command(command);
+		return (NULL);
+	}
 	return (command);
 }
 
 void	free_command(void *command)
 {
 	free(command);
+}
+
+static t_command	*set_command_default_files(t_command *command)
+{
+	t_dll		**node_ptr;
+	t_file		*file;
+
+	command->in_files = NULL;
+	command->out_files = NULL;
+	node_ptr = &command->in_files;
+	while (node_ptr != NULL)
+	{
+		file = new_file(NULL, FILE_STD, 0);
+		if (file == NULL)
+			return (NULL);
+		add_dll(node_ptr, new_dll(file));
+		if (*node_ptr == NULL)
+		{
+			free_file(file);
+			return (NULL);
+		}
+		if (*node_ptr == command->in_files)
+			node_ptr = &command->out_files;
+		else
+			node_ptr = NULL;
+	}
+	return (command);
 }
