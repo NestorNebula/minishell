a7471d8c237641dc888a33918b7c979648dba20d
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-19 11:18:08 +0100

    feat: update cd builtin to use getcwd function

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -10,20 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <errno.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include "builtins.h"
 #include "env.h"
 
 static int	cd(const char *path);
 
-static void	update_pwd(t_shell *shell, const char *pwd);
+static void	update_pwd(t_shell *shell, const char *oldpwd, const char *pwd);
 
-int				builtin_cd(t_command *command, t_shell *shell)
+int	builtin_cd(t_command *command, t_shell *shell)
 {
 	int		rc;
+	char	*old_cwd;
+	char	*cwd;
 
+	if (command->args[1] == NULL)
+		return (0);
+	old_cwd = getcwd(NULL, 0);
 	rc = cd(command->args[1]);
-	update_pwd(shell, command->args[1]);
+	if (rc != 0)
+		rc = errno;
+	cwd = getcwd(NULL, 0);
+	if (rc == 0)
+		update_pwd(shell, old_cwd, cwd);
+	free(old_cwd);
+	free(cwd);
 	return (rc);
 }
 
@@ -32,13 +45,10 @@ static int	cd(const char *path)
 	return (chdir(path));
 }
 
-static void	update_pwd(t_shell *shell, const char *pwd)
+static void	update_pwd(t_shell *shell, const char *oldpwd, const char *pwd)
 {
-	t_env_var	*old_pwd;
-
-	old_pwd = find_env_var(shell->env, "PWD");
-	if (old_pwd != NULL)
-		set_env_var(&shell->env, "OLDPWD", old_pwd->value);
+	if (oldpwd != NULL)
+		set_env_var(&shell->env, "OLDPWD", (char *) oldpwd);
 	if (pwd != NULL)
 		set_env_var(&shell->env, "PWD", (char *) pwd);
 }
