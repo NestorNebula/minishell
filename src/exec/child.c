db453d197ce41b0a106dba741747a0acf2600431
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-21 11:46:20 +0100

    feat: replace close_fds function by close_files

--- src/exec/child.c
+++ src/exec/child.c
@@ -14,12 +14,9 @@
 #include <unistd.h>
 #include "exec.h"
 #include "env.h"
-#include "file.h"
 #include "process.h"
 
 static void	exit_from_child(int ec, t_dll *command_node, t_shell *shell);
-
-static void	close_fds(t_dll *command_node);
 
 int	handle_child(t_dll *command_node, t_shell *shell)
 {
@@ -31,48 +28,22 @@ int	handle_child(t_dll *command_node, t_shell *shell)
 	command = command_node->data;
 	close(command->pipe[0]);
 	ec = prepare_command(command_node);
+	close_files(command_node);
 	if (ec != 0)
-		exit_from_child(ec, command_node, shell);
+		exit_from_child(ec, NULL, shell);
 	ec = exec_command(command, shell);
-	exit_from_child(ec, command_node, shell);
+	exit_from_child(ec, NULL, shell);
 	exit(ec);
 }
 
 static void	exit_from_child(int ec, t_dll *command_node, t_shell *shell)
 {
 	if (command_node != NULL)
-		close_fds(command_node);
+		close_files(command_node);
 	if (shell != NULL)
 	{
 		clear_dll(&shell->cmds, free_command);
 		clear_dll(&shell->env, free_env_var);
 	}
 	exit(ec);
-}
-
-static void	close_fds(t_dll *command_node)
-{
-	t_command	*command;
-	t_command	*prev;
-	t_file		*last_in;
-	t_file		*last_out;
-
-	command = command_node->data;
-	prev = NULL;
-	if (command_node->prev != NULL && command_node->prev->data != NULL)
-		prev = command_node->prev->data;
-	last_in = NULL;
-	if (command->in_files != NULL)
-		last_in = dll_last(command->in_files)->data;
-	last_out = NULL;
-	if (command->out_files != NULL)
-		last_out = dll_last(command->out_files)->data;
-	if (last_in != NULL && last_in->type == FILE_PIPE && prev != NULL)
-		close(prev->pipe[0]);
-	else if (last_in != NULL && last_in->type)
-		close(last_in->fd);
-	if (last_out != NULL && last_out->type == FILE_PIPE)
-		close(command->pipe[1]);
-	else if (last_out != NULL)
-		close(last_out->fd);
 }
