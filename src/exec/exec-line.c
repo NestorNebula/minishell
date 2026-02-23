acba80f731f1fd63f94271196bd06b7db608b74d
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-23 09:09:33 +0100

    feat: add exec_line function

---
+++ src/exec/exec-line.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec-line.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 07:52:04 by nhoussie          #+#    #+#             */
+/*   Updated: 2026/02/23 08:44:07 by nhoussie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "exec.h"
+
+int		exec_line(t_dll *commands, t_shell *shell)
+{
+	int	rc;
+
+	(void) shell;
+	while (commands != NULL)
+	{
+		rc = handle_command(commands, shell);
+		commands = commands->next;
+	}
+	return (rc);
+}
