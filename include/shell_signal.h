2e630891f0404ea3430561ad6374330174bd98b7
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-24 16:05:16 +0100

    feat: add signal module's header

---
+++ include/shell_signal.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shell_signal.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 15:09:38 by nhoussie          #+#    #+#             */
+/*   Updated: 2026/02/24 16:03:13 by nhoussie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHELL_SIGNAL_H
+# define SHELL_SIGNAL_H
+
+/**
+ * Initializes program's signal setup.
+ */
+void	init_signals(void);
+
+#endif // !SHELL_SIGNAL_H
