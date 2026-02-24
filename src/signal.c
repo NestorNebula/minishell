1e6d9e42f2c1f2b08904a8100a59f3b023ffed7c
Cedric Monmirel <cedric.monmirel@gmail.com>
2026-02-24 07:06:55 +0100

    signaux

---
+++ src/signal.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmonmire <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/02 14:14:09 by cmonmire          #+#    #+#             */
+/*   Updated: 2026/02/02 14:46:22 by cmonmire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shell.h"
+
+volatile sig_atomic_t	g_signal;
+
+void	sig_handler(int sig)
+{
+	g_signal = sig;
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void	init_signals(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_handler = sig_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa, NULL);
+	signal(SIGQUIT, SIG_IGN);
+}
