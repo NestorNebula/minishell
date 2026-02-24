9fd18ec1b83a629f4f682de25b9d99a1bd0c6946
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-24 16:16:58 +0100

    fix: add missing header inclusion

--- include/shell.h
+++ include/shell.h
@@ -6,12 +6,14 @@
 /*   By: cmonmire <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 12:11:39 by cmonmire          #+#    #+#             */
-/*   Updated: 2026/02/05 12:19:19 by cmonmire         ###   ########.fr       */
+/*   Updated: 2026/02/24 16:16:37 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHELL_H
 # define SHELL_H
+
+# include "dll.h"
 
 typedef struct s_shell
 {
