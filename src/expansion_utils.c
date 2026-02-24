7b7bbd5deda073d9b482253320fa86ae20ed928f
Cedric Monmirel <cedric.monmirel@gmail.com>
2026-02-24 06:58:18 +0100

    src expansion

---
+++ src/expansion_utils.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expansion_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmonmire <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/18 08:01:29 by cmonmire          #+#    #+#             */
+/*   Updated: 2026/02/19 07:09:51 by cmonmire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shell.h"
+#include "env.h"
+
+char	*append_str(char *s1, char *s2)
+{
+	char	*new;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (s1);
+	new = ft_strjoin(s1, s2);
+	free(s1);
+	return (new);
+}
+
+char	*append_char(char *s, char c)
+{
+	char	tmp[2];
+
+	tmp[0] = c;
+	tmp[1] = '\0';
+	return (append_str(s, tmp));
+}
+
+char	*read_var_name(char *str, int *i)
+{
+	int	start;
+
+	start = *i;
+	while (str[*i] && (ft_isalnum(str[*i]) || str[*i] == '_'))
+		(*i)++;
+	return (ft_substr(str, start, *i - start));
+}
+
+char	*get_env_value(t_shell *shell, char *key)
+{
+	t_dll		*tmp;
+	t_env_var	*var;
+
+	tmp = shell->env;
+	while (tmp)
+	{
+		var = (t_env_var *)tmp->data;
+		if (!ft_strncmp(var->key, key, ft_strlen(key) + 1) == 0)
+			return (var->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
+
+char	*remove_quotes(char *str)
+{
+	char	*result;
+	int		i;
+	int		j;
+	char	quote;
+
+	if (!str)
+		return (NULL);
+	result = malloc(ft_strlen(str) + 1);
+	if (!result)
+		return (NULL);
+	i = 0;
+	j = 0;
+	quote = 0;
+	while (str[i])
+	{
+		if ((str[i] == '\'' || str[i] == '"') && quote == 0)
+			quote = str[i];
+		else if (str[i] == quote)
+			quote = 0;
+		else
+			result[j++] = str[i];
+		i++;
+	}
+	result[j] = '\0';
+	return (result);
+}
