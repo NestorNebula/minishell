276a6c060692ac1da96e322662e61573ff48c990
Noa Houssier <nestornebula.dev@gmail.com>
2026-02-27 10:03:16 +0100

    feat: update heredoc function calls arguments

--- src/exec/exec-line.c
+++ src/exec/exec-line.c
@@ -6,7 +6,7 @@
 /*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 07:52:04 by nhoussie          #+#    #+#             */
-/*   Updated: 2026/02/23 08:44:07 by nhoussie         ###   ########.fr       */
+/*   Updated: 2026/02/27 09:35:18 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	exec_line(t_dll *commands, t_shell *shell)
 	int	rc;
 
 	(void) shell;
-	rc = prepare_heredocs(commands);
+	rc = prepare_heredocs(commands, shell);
 	if (rc != 0)
 		return (rc);
 	while (commands != NULL)
