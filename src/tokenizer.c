3351a056cc230791e75fd1bbf828375017c4fe50
Noa Houssier <nestornebula.dev@gmail.com>
2026-03-01 15:25:17 +0100

    fix: handle allocation errors in tokenizer

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -6,82 +6,73 @@
 /*   By: cmonmire <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 09:06:21 by cmonmire          #+#    #+#             */
-/*   Updated: 2026/02/11 11:57:10 by cmonmire         ###   ########.fr       */
+/*   Updated: 2026/03/01 15:00:59 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 #include "token.h"
-
-static char	*read_word(char *line, int *i)
-{
-	int		start;
-	char	quote;
-
-	start = *i;
-	quote = 0;
-	while (line[*i])
-	{
-		if (!quote && (line[*i] == '\'' || line[*i] == '"'))
-			quote = line[*i];
-		else if (quote && line[*i] == quote)
-			quote = 0;
-		else if (!quote && (line[*i] == ' ' || line[*i] == '\t'
-				|| line[*i] == '|' || line[*i] == '<' || line[*i] == '>'))
-			break ;
-		(*i)++;
-	}
-	return (ft_substr(line, start, *i - start));
-}
 
 static int	is_operator(char c)
 {
 	return (c == '|' || c == '<' || c == '>');
 }
 
-static void	one_operator(t_dll **tokens, char *line, int *i)
+static t_dll	*one_operator(char *line, int *i)
 {
+	t_dll	*tok_node;
 	t_token	*tok;
 
 	if (line[*i] == '<')
-	{
 		tok = new_token(T_REDIR_IN, ft_strdup("<"));
-		(*i)++;
-	}
 	else if (line[*i] == '>')
-	{
 		tok = new_token(T_REDIR_OUT, ft_strdup(">"));
-		(*i)++;
-	}
 	else
-	{
 		tok = new_token(T_PIPE, ft_strdup("|"));
-		(*i)++;
+	(*i)++;
+	if (tok == NULL)
+		return (NULL);
+	if (tok->value == NULL)
+	{
+		free(tok);
+		return (NULL);
 	}
-	add_dll(tokens, new_dll(tok));
+	tok_node = new_dll(tok);
+	if (tok_node == NULL)
+		free_token(tok);
+	return (tok_node);
 }
 
-static void	double_operator(t_dll **tokens, char *line, int *i)
+static t_dll	*double_operator(char *line, int *i)
 {
+	t_dll	*tok_node;
 	t_token	*tok;
 
 	if (line[*i] == '<' && line[*i + 1] == '<')
-	{
 		tok = new_token(T_HEREDOC, ft_strdup("<<"));
-		*i += 2;
-	}
 	else
-	{
 		tok = new_token(T_REDIR_APPEND, ft_strdup(">>"));
-		*i += 2;
+	*i += 2;
+	if (tok == NULL)
+		return (NULL);
+	if (tok->value == NULL)
+	{
+		free(tok);
+		return (NULL);
 	}
-	add_dll(tokens, new_dll(tok));
+	tok_node = new_dll(tok);
+	if (tok_node == NULL)
+		free_token(tok);
+	return (tok_node);
 }
+
+static t_dll	*handle_token(char *line, int *i);
 
 t_dll	*tokenizer(char *line)
 {
 	t_dll	*tokens;
-	t_token	*tok;
+	t_dll	*tok_node;
 	int		i;
 
 	tokens = NULL;
@@ -90,18 +81,45 @@ t_dll	*tokenizer(char *line)
 	{
 		if (line[i] == ' ' || line[i] == '\t')
 			i++;
-		else if (is_operator(line[i]))
-		{
-			if (line[i] == line[i + 1])
-				double_operator(&tokens, line, &i);
-			else
-				one_operator(&tokens, line, &i);
-		}
 		else
 		{
-			tok = new_token(T_WORD, read_word(line, &i));
-			add_dll(&tokens, new_dll(tok));
+			tok_node = handle_token(line, &i);
+			if (tok_node == NULL)
+			{
+				clear_dll(&tokens, free_token);
+				return (NULL);
+			}
+			add_dll(&tokens, tok_node);
 		}
 	}
 	return (tokens);
+}
+
+static t_dll	*handle_token(char *line, int *i)
+{
+	t_dll	*tok_node;
+	t_token	*tok;
+
+	if (is_operator(line[*i]))
+	{
+		if (line[*i] == line[*i + 1] && line[*i] != '|')
+			tok_node = double_operator(line, i);
+		else
+			tok_node = one_operator(line, i);
+	}
+	else
+	{
+		tok = new_token(T_WORD, read_word(line, i));
+		if (tok == NULL)
+			return (NULL);
+		if (tok->value == NULL)
+		{
+			free(tok);
+			return (NULL);
+		}
+		tok_node = new_dll(tok);
+		if (tok_node == NULL)
+			free_token(tok);
+	}
+	return (tok_node);
 }
