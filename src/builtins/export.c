075596ca826a0affe1a15c456e87af301d040e73
Noa Houssier <nhoussie@student.42.fr>
2026-03-02 10:45:43 +0100

    feat: add handle_no_arg for builtin_export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -16,11 +16,14 @@
 #include <unistd.h>
 #include "builtins.h"
 #include "env.h"
+#include "file.h"
 #include "libft.h"
 
 static bool	is_valid_key(const char *key);
 
 static int	export_env_var(t_shell *shell, char *env_str);
+
+static int	handle_no_arg(t_command *command, t_shell *shell);
 
 int	builtin_export(t_command *command, t_shell *shell)
 {
@@ -29,6 +32,8 @@ int	builtin_export(t_command *command, t_shell *shell)
 	size_t	i;
 
 	error = false;
+	if (command->args[1] == NULL)
+		return (handle_no_arg(command, shell));
 	i = 1;
 	while (command->args[i] != NULL)
 	{
@@ -79,6 +84,34 @@ static int	export_env_var(t_shell *shell, char *env_str)
 	{
 		rc = errno;
 		ft_dprintf(STDERR_FILENO, "export: %s\n", strerror(rc));
+		return (rc);
+	}
+	return (0);
+}
+
+static int	handle_no_arg(t_command *command, t_shell *shell)
+{
+	int			rc;
+	int			fd;
+	t_dll		*env_ptr;
+	t_env_var	*env_var;
+
+	rc = 0;
+	fd = STDOUT_FILENO;
+	if (dll_size(shell->cmds) == 1)
+		fd = ((t_file *) dll_last(command->out_files)->data)->fd;
+	env_ptr = shell->env;
+	while (env_ptr != NULL)
+	{
+		env_var = env_ptr->data;
+		if (env_var != NULL)
+			rc = ft_dprintf(fd, "export %s=%s\n", env_var->key, env_var->value);
+		env_ptr = env_ptr->next;
+	}
+	if (rc == -1)
+	{
+		rc = errno;
+		ft_dprintf(STDERR_FILENO, "env: %s\n", strerror(errno));
 		return (rc);
 	}
 	return (0);
