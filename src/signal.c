d3e91c9073cfb8be48440573e293aaec021ab4a6
Noa Houssier <nestornebula.dev@gmail.com>
2026-03-03 11:21:10 +0100

    Signal (#17)
    * token
    
    * Correction tokenizer.c && suppression type.h
    
    * Correction tokenizer.c && suppression type.h
    
    * Cette fois c est la bonne
    
    * src expansion
    
    * envp_to_env et vice versa
    
    * signaux
    
    * chore: remove off-topic files
    
    * fix: add missing header inclusion
    
    * fix: add missing headers inclusion
    
    * feat: add signal module's header
    
    * feat: add signal functions prototypes
    
    * feat: add signal handlers and setters
    
    * fix: add definition for NULL
    
    ---------
    
    Co-authored-by: Cedric Monmirel <cedric.monmirel@gmail.com>

---
+++ src/signal.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmonmire <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/02 14:14:09 by cmonmire          #+#    #+#             */
+/*   Updated: 2026/02/26 14:22:27 by nhoussie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <signal.h>
+#include <stddef.h>
+#include "shell_signal.h"
+
+static void	set_signals(void (*handler)(int));
+
+void	set_default_signals(void)
+{
+	set_signals(sig_handler);
+}
+
+void	set_heredoc_signals(void)
+{
+	set_signals(heredoc_sig_handler);
+}
+
+void	set_exec_signals(void)
+{
+	set_signals(exec_sig_handler);
+}
+
+static void	set_signals(void (*handler)(int))
+{
+	struct sigaction	sa;
+
+	sa.sa_handler = handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa, NULL);
+	signal(SIGQUIT, SIG_IGN);
+}
